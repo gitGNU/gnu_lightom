6ea4ff535779531fd5bb0cd49c729779417227c1
Daniel Pimentel <d4n1@d4n1.org>
2017-01-09 21:24:48 -0300

    add events map

--- lightom/web/static/web/js/lightomap.js
+++ lightom/web/static/web/js/lightomap.js
@@ -2,19 +2,32 @@ var lightomap;
 var marker;
 var cirle;
 var html;
+var json;
 var popup = L.popup();
 
-function initLightomap() {
-    lightomap = new L.map('lightomap').setView([-9.6565863, -35.7384311], 8);
+function initLightomap(e) {
+    lightomap = new L.map('lightomap').setView([-9.6565863, -35.7384311], 13);
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '<a href="http://openstreetmap.org">Map</a>',
 	maxZoom: 18}).addTo(lightomap);
-    
-    html = "<b>ID:</b> 1234<br> <b>Nome:</b> Innovate <br/> <b>Estado:</b> <a href='#'>Desligar</a><br> <b>Dimerização:</b> <a href='#'>100%</a><br/> <b>Potência:</b> 80%<br> <b>Tensão:</b> 40%<br>"
-    marker = L.marker([-9.6565863, -35.7384311]).addTo(lightomap);
-    marker.bindPopup(html);
+    lightomap.doubleClickZoom.disable();
+}
 
-    circle = L.circle([-9.6565863, -35.7384311], {
+function addPoint(j) {
+    html = "<b>Id:</b> " + j.Id + "<br/>" +
+	"<b>Nome:</b> " + j.Nome + "<br/>" +
+	"<b>Estado:</b> <a href='#'> " + j.Estado + "</a><br>" +
+	"<b>Dimerização:</b> <a href='#'> " + j.Dismerizar + "%</a><br/>" +
+	"<b>Potência:</b> " + j.Potencia + "<br/>" +
+	"<b>Tensão:</b> " + j.Tensao + "<br/>" +
+	"<b>Latitude:</b> " + j.Latitude + "<br/>" +
+	"<b>Longitude:</b> " + j.Longitude + "<br/>"; 
+    marker = L.marker([j.Latitude, j.Longitude]).addTo(lightomap);
+    marker.bindPopup(html);
+}
+
+function addCircle(j) {
+    circle = L.circle([j.Latitude, j.Longitude], {
 	color: 'red',
 	fillColor: '#f03',
 	fillOpacity: 0.5,
@@ -23,10 +36,20 @@ function initLightomap() {
     circle.bindPopup("Setor de cobertura");
 }
 
-function clickLightom(e) {
-    popup
-        .setLatLng(e.latlng)
-        .setContent("Deseja incluir um ponto de iluminação nessa coordenada (" +
-		    e.latlng.toString() + ")? <br/><a href='#'>Sim</a><br/>")
-        .openOn(lightomap);
+function clickMap(e) {
+    json = {"Id": "1234",
+	    "Nome": "Innovate",
+	    "Estado": "Desligado",
+	    "Dismerizar": "100",
+	    "Potencia": "100",
+	    "Tensao": "100",
+	    "Latitude": e.latlng.lat.toString(),
+	    "Longitude": e.latlng.lng.toString()};
+    addPoint(json);
+}
+
+function doubleClickMap(e) {
+    json = {"Latitude": e.latlng.lat.toString(),
+	    "Longitude": e.latlng.lng.toString()};
+    addCircle(json);
 }
