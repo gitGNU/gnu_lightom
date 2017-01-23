cc665fe7bd4c6a1cdc18217e65c4045022a6fc7a
Daniel Pimentel <d4n1@d4n1.org>
2017-01-23 17:23:38 -0300

    update views

--- lightom/web/static/web/js/lightomap.js
+++ lightom/web/static/web/js/lightomap.js
@@ -1,45 +1,42 @@
 var lightomap;
-var marker;
-var cirle;
-var html;
-var json;
-var popup = L.popup();
 
-function initLightomap(data) {
+function initLightomap() {
     lightomap = new L.map('lightomap').setView([-9.6565863, -35.7384311], 13);
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '<a href="http://openstreetmap.org">Map</a>',
 	maxZoom: 18}).addTo(lightomap);
     lightomap.doubleClickZoom.disable();
-
-   /*
 }
 
 function LoadConcentrador(data) {
-    console.log(data);
-*/
-    html = "<b>Serial:</b> " + data.Serial + "<br/>" +
+    var html = "<b>Serial:</b> " + data.Serial + "<br/>" +
 	"<b>Latitude:</b> " + data.Latitude + "<br/>" +
 	"<b>Longitude:</b> " + data.Longitude + "<br/>" +
 	"<b>IP:</b> " + data.IP + "<br/>" +
-	"<b>Atualização:</b> " + data.Atualizacao + "<br/>"; 
-    marker = L.marker([data.Latitude, data.Longitude]).addTo(lightomap);
+	"<b>Atualização:</b> " + data.Atualizacao + "<br/>" +
+	"<a href='/web/concentrador/" + data.Serial +
+	"'>Acessar dispositivos vinculados</a>"; 
+    var marker = L.marker([data.Latitude, data.Longitude]).addTo(lightomap);
     marker.bindPopup(html);
 }
 
-/*
 function AddConcentrador(event) {
-    json = {"Serial": "#83GNU$",
+    var data = {"Serial": "#83GNU$",
 	    "Latitude": event.latlng.lat.toString(),
 	    "Longitude": event.latlng.lng.toString(),
-	    "IP": "10.10.10.1",
-	    "Atualizacao": "11/10/2017 - 13:00:00"};
-    marker = L.marker([json.Latitude, json.Longitude]).addTo(lightomap);
-    //marker.bindPopup(html);
+	    "IP": "10.0.0.1",
+		"Atualizacao": "11/10/2017 - 13:00:00"};
+    var html = "<b>Serial:</b> " + data.Serial + "<br/>" +
+	"<b>Latitude:</b> " + data.Latitude + "<br/>" +
+	"<b>Longitude:</b> " + data.Longitude + "<br/>" +
+	"<b>IP:</b> " + data.IP + "<br/>" +
+	"<b>Atualização:</b> " + data.Atualizacao + "<br/>";
+    var marker = L.marker([data.Latitude, data.Longitude]).addTo(lightomap);
+    marker.bindPopup(html);
 }
 
 function LoadDispositivo(data) {
-    html = "<b>Serial:</b> " + data.Serial + "<br/>" +
+    var html = "<b>Serial:</b> " + data.Serial + "<br/>" +
 	"<b>Estado:</b> <a href='#'> " + data.Estado + "</a><br>" +
 	"<b>Dimerização:</b> <a href='#'> " + data.Dismerizacao + "%</a><br/>" +
 	"<b>Potência:</b> " + data.Potencia + "<br/>" +
@@ -49,11 +46,12 @@ function LoadDispositivo(data) {
 	"<b>Latitude:</b> " + data.Latitude + "<br/>" +
 	"<b>Longitude:</b> " + data.Longitude + "<br/>" +
 	"<b>IP:</b> " + data.IP + "<br/>" +
-	"<b>Atualização:</b> " + data.Atualizacao + "<br/>"; 
-    marker = L.marker([data.Latitude, data.Longitude]).addTo(lightomap);
+	"<b>Atualização:</b> " + data.Atualizacao + "<br/>";
+    var marker = L.marker([data.Latitude, data.Longitude]).addTo(lightomap);
     marker.bindPopup(html);
 }
 
+/*
 function addCircle(j) {
     circle = L.circle([j.Latitude, j.Longitude], {
 	color: 'red',
